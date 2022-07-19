3ca60405699eb2a42cd21d12ae1966188850d59e
KamilMammadov <kamiltm@code.edu.az>
2022-07-19 21:35:00 +0400

    abstract weapons created, Shoot,reload,kit methods created

---
+++ SimpleGame/SimpleGame/Database/Models/Weapons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGame.Database.Models
+{
+    abstract class Weapons
+    {
+        public abstract void Shoot();
+
+        public abstract void Reload();
+
+        public abstract void Kit();
+
+    }
+}
